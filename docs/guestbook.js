c90dfa77d8eb91ec14fb9de9dfe46998f587b8da
kmaduna <55139448+kmaduna@users.noreply.github.com>
2019-09-17 17:14:17 +0200

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://4387882d.us-south.apiconnect.appdomain.cloud';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
